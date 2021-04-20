48a2f26e7191cae04337e16f795a0a41382d4b11
unknown <ferreirazaltan@gmail.com>
2021-04-20 15:34:42 -0300

    primeiro commit

---
+++ service-email/src/main/java/br/com/orandetalents/ecommerce/Email.java
@@ -0,0 +1,11 @@
+package br.com.orandetalents.ecommerce;
+
+public class Email {
+
+    private final String subject, body;
+
+    public Email(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+}
